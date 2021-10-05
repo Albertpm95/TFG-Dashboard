b54d8908669c7aa907d2437bc831b36ac490f4f2
Albertpm95 <Albertpm95@gmail.com>
2021-10-05 16:24:19 +0200

    Añadidos los primeros 3 componentes principales y eliminados los spec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './core/principal/principal.component';
+import { QRComponent } from './core/qr/qr.component';
+import { InfoExtraComponent } from './core/info-extra/info-extra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    QRComponent,
+    InfoExtraComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/info-extra/info-extra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-extra',
+  templateUrl: './info-extra.component.html',
+  styleUrls: ['./info-extra.component.scss']
+})
+export class InfoExtraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
